6a2577414d0902faac9ec1067513e6b6c865a7a3
Fernando Valverde <nandovalverde@gmail.com>
2022-06-24 21:06:52 +0100

    Criado o app base

--- form/urls.py
+++ form/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from form.base.views import home
-
 urlpatterns = [
+    path('', include('base.urls')),
     path('admin/', admin.site.urls),
-    path('base/', include('base.urls')),
+
 ]
