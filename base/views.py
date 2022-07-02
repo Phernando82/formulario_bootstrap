d13a1dea0a295291532439ffa03984a2e720c4cc
Fernando Valverde <nandovalverde@gmail.com>
2022-07-02 19:44:26 +0100

    Instalada a Django Debug Toolbar

--- base/views.py
+++ base/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Olá Django!')
+    return HttpResponse('<html><body>Olá Django!</body></html>', content_type='text/html')
--- form/settings.py
+++ form/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
-from decouple import config
+from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,6 +73,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'form.wsgi.application'
+
+# configuração Django Debug Toolbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
