e8153650d157371037557de64f5035dcd720d891
Fernando Valverde <nandovalverde@gmail.com>
2022-07-09 20:11:47 +0100

    Merge remote-tracking branch 'origin/dev' into dev

--- form/settings.py
+++ form/settings.py
@@ -144,7 +144,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
-    AWS_DEFAULT_ACL = 'private'
+    AWS_DEFAULT_ACL = None
 
     # Static Assets
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
